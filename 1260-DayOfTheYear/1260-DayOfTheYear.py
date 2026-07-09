f741505b48e69341c6bdb02b1e9c551c911a2a1d
priyan2627 <priyanraj2627@gmail.com>
2026-07-09 10:21:08 +0530

    Create 1260-DayOfTheYear

---
+++ 1260-DayOfTheYear/1260-DayOfTheYear.py
@@ -0,0 +1,12 @@
+# Last updated: 7/9/2026, 10:21:08 AM
+class Solution:
+    def dayOfYear(self, date: str) -> int:
+        year, month, day = map(int, date.split('-'))
+
+        days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+        # Check for leap year
+        if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
+            days[1] = 29
+
+        return sum(days[:month - 1]) + day
