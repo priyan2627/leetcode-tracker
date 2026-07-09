04291f2d3fe744da3ef7f828dfb3d3e8277d9f7a
priyan2627 <priyanraj2627@gmail.com>
2026-07-09 10:21:15 +0530

    Create 645-SetMismatch

---
+++ 645-SetMismatch/645-SetMismatch.py
@@ -0,0 +1,15 @@
+# Last updated: 7/9/2026, 10:21:16 AM
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        seen = set()
+        duplicate = -1
+
+        for num in nums:
+            if num in seen:
+                duplicate = num
+            else:
+                seen.add(num)
+
+        for i in range(1, len(nums) + 1):
+            if i not in seen:
+                return [duplicate, i]
