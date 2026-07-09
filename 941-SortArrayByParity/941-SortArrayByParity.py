50cbcf3879b0c45e555b136d5adbaf1bafd3909f
priyan2627 <priyanraj2627@gmail.com>
2026-07-09 10:21:14 +0530

    Create 941-SortArrayByParity

---
+++ 941-SortArrayByParity/941-SortArrayByParity.py
@@ -0,0 +1,17 @@
+# Last updated: 7/9/2026, 10:21:14 AM
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        left = 0
+        right = len(nums) - 1
+
+        while left < right:
+            if nums[left] % 2 > nums[right] % 2:
+                nums[left], nums[right] = nums[right], nums[left]
+
+            if nums[left] % 2 == 0:
+                left += 1
+
+            if nums[right] % 2 == 1:
+                right -= 1
+
+        return nums
