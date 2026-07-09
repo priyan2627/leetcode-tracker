ac487b653b42ee1d26388084c4aa9184a5b316cc
priyan2627 <priyanraj2627@gmail.com>
2026-07-09 10:21:12 +0530

    Create 958-SortArrayByParityIi

---
+++ 958-SortArrayByParityIi/958-SortArrayByParityIi.py
@@ -0,0 +1,18 @@
+# Last updated: 7/9/2026, 10:21:12 AM
+class Solution:
+    def sortArrayByParityII(self, nums: List[int]) -> List[int]:
+        i = 0
+        j = 1
+        n = len(nums)
+
+        while i < n and j < n:
+            if nums[i] % 2 == 0:
+                i += 2
+            elif nums[j] % 2 == 1:
+                j += 2
+            else:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 2
+                j += 2
+
+        return nums
