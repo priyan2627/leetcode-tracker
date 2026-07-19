54a217bc3729037479fe216bc959c9f59d1204f2
priyan2627 <priyanraj2627@gmail.com>
2026-07-19 09:16:37 +0530

    Create

---
+++ problem-1784432797844/version/python/problem-1784432797844_20260719_091637.py
@@ -0,0 +1,4 @@
+# Last updated: 7/19/2026, 9:16:37 AM
+1class Solution:
+2    def canReach(self, start: list[int], target: list[int]) -> bool:
+3        return (start[0]+start[1])%2==(target[0]+target[1])%2
