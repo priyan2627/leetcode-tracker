59946276ac28ab3d79bf8c82f03c1dfba69fedf6
priyan2627 <priyanraj2627@gmail.com>
2026-07-28 13:06:53 +0530

    Create 22-GenerateParentheses

---
+++ 22-GenerateParentheses/version/python/22-GenerateParentheses_20260728_130652.py
@@ -0,0 +1,11 @@
+# Last updated: 7/28/2026, 1:06:52 PM
+1class Solution:
+2    def strStr(self, haystack, needle):
+3        n = len(haystack)
+4        m = len(needle)
+5
+6        for i in range(n - m + 1):
+7            if haystack[i:i + m] == needle:
+8                return i
+9
+10        return -1
