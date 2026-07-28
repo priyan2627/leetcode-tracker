64029d4f6dd959de3df6fddec94af89cfe51d4c7
priyan2627 <priyanraj2627@gmail.com>
2026-07-28 12:24:55 +0530

    Create 17-LetterCombinationsofaPhoneNumber

---
+++ 17-LetterCombinationsofaPhoneNumber/version/python/17-LetterCombinationsofaPhoneNumber_20260728_122454.py
@@ -0,0 +1,19 @@
+# Last updated: 7/28/2026, 12:24:54 PM
+1class Solution:
+2    def isValid(self, s):
+3        stack = []
+4        mapping = {
+5            ')': '(',
+6            '}': '{',
+7            ']': '['
+8        }
+9
+10        for char in s:
+11            if char in mapping:
+12                if not stack or stack[-1] != mapping[char]:
+13                    return False
+14                stack.pop()
+15            else:
+16                stack.append(char)
+17
+18        return len(stack) == 0
