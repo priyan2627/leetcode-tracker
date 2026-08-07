6ae5e920fb86f48371552d37b4b4c1fb2e1329e4
priyan2627 <priyanraj2627@gmail.com>
2026-08-07 15:55:13 +0530

    Create 49-GroupAnagrams

---
+++ 49-GroupAnagrams/version/python/49-GroupAnagrams_20260807_155510.py
@@ -0,0 +1,14 @@
+# Last updated: 8/7/2026, 3:55:10 PM
+1class Solution:
+2    def groupAnagrams(self, strs):
+3        groups = {}
+4
+5        for word in strs:
+6            key = ''.join(sorted(word))
+7
+8            if key not in groups:
+9                groups[key] = []
+10
+11            groups[key].append(word)
+12
+13        return list(groups.values())
