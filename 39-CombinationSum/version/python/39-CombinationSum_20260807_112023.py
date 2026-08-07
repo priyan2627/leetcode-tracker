9ce72f34977865aefacfcb1bbc99c9211506aa62
priyan2627 <priyanraj2627@gmail.com>
2026-08-07 11:20:26 +0530

    Create 39-CombinationSum

---
+++ 39-CombinationSum/version/python/39-CombinationSum_20260807_112023.py
@@ -0,0 +1,24 @@
+# Last updated: 8/7/2026, 11:20:23 AM
+1class Solution:
+2    def combinationSum(self, candidates, target):
+3        result = []
+4
+5        def backtrack(start, current, total):
+6            if total == target:
+7                result.append(current[:])
+8                return
+9
+10            if total > target:
+11                return
+12
+13            for i in range(start, len(candidates)):
+14                current.append(candidates[i])
+15
+16                # i instead of i + 1 because we can reuse the same number
+17                backtrack(i, current, total + candidates[i])
+18
+19                current.pop()
+20
+21        backtrack(0, [], 0)
+22
+23        return result
